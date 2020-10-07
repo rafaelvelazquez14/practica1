83f2afeb292235f56d130c7a6e1a239e711e2e7d
aabrilvg <72525069+aabrilvg@users.noreply.github.com>
2020-10-07 16:30:56 -0500

    Update Holamundo2.c

--- Holamundo2.c
+++ Holamundo2.c
@@ -1,7 +1,7 @@
 #include<stdio.h> int main(){
 printf("Este es mi segundo codigo"); printf("Programar es facil");
 printf("Voy a compartir con mi compañero el codigo");
-printf("\nAbri Vázquez\n");
+printf("\nAbril Vázquez\n");
 return 0;
 }
 
