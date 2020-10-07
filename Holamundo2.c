86338f5cd8fc599f201085d6aeda0352f059256b
rafaelvelazquez14 <72525258+rafaelvelazquez14@users.noreply.github.com>
2020-10-07 16:16:59 -0500

    Update and rename README.md to Holamundo2.c

---
+++ Holamundo2.c
@@ -0,0 +1,6 @@
+#include<stdio.h> int main(){
+printf("Este es mi segundo codigo"); printf("Programar es facil");
+printf("Voy a compartir con mi compañero el codigo"); return 0;
+}
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# practica1
-Prueba 
